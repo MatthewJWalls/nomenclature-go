cb99436d4050135a23664afcf60fc0a59dee3234
Matthew Walls <matthewjwalls@googlemail.com>
2016-04-26 10:15:08 +0100

    Naive implementation of file reading

--- README.md
+++ README.md
@@ -7,6 +7,12 @@ are guaranteed unique. Useful for naming servers, databases, etc.
 
 This is a work in progress and is still missing key features for usability.
 
+Usage:
+
+    nomenclature -f1 prefixes.txt -f2 postfixes.txt
+
+After which it will serve names on port 80
+    
 Todo:
 * ~~name generation~~
 * definable input lists
--- nomenclature.go
+++ nomenclature.go
@@ -2,20 +2,55 @@ package main
 
 import (
 	"log"
+	"flag"
+	"strings"
 	"net/http"
+	"io/ioutil"
 	
 	"github.com/MatthewJWalls/nomenclature/server"
 	"github.com/MatthewJWalls/nomenclature/generator"
 )
 
+// This is the naive approach for loading the word lists.
+// In the future we may want to deal with potentially large
+// lists which will need a bit of a rethink here so we're
+// only reading in the bytes we want.
+
+func readWordList(path string) ([]string) {
+
+	data, err := ioutil.ReadFile(path)
+
+	if err != nil {
+		log.Fatalf("Failed to read word list %q", path)
+	}
+
+	raw := strings.Split(string(data), "\n")
+	out := []string{}
+
+	for i := 0; i < len(raw); i++ {
+		if strings.TrimSpace(raw[i]) != "" {
+			out = append(out, strings.TrimSpace(raw[i]))
+		}
+	}
+
+	return out
+
+}
 
 func main() {
 
+	log.Printf("Initialising data")
+
+	prefile := flag.String("f1", "pre", "path to prefix words file")
+	postfile := flag.String("f2", "post", "path to prefix words file")
+
+	flag.Parse()
+	
+	pre := readWordList(*prefile)
+	pst := readWordList(*postfile)
+	
 	log.Printf("Starting server")
 	
-	pre := []string{"Big", "Bad", "Horrible"}
-	pst := []string{"Wolf", "Chicken", "Hernia"}
-
 	a := generator.NewStandardGenerator(pre, pst)
 	
 	http.HandleFunc("/", server.NewWebHandler(a))

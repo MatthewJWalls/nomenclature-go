55f53715b159100564af9cbe9362dec8abbc2c35
Matthew Walls <matthewjwalls@googlemail.com>
2016-04-27 07:50:12 +0100

    Concurrent access mechanism

--- server/server.go
+++ server/server.go
@@ -11,12 +11,24 @@ type Generator interface {
 	Next() string
 }
 
+// Given a generator and a channel, fills the channel with
+// the results of generator.Next(). This is our basic thread
+// safety mechanism.
+func pumpGeneratorIntoChannel(gen Generator, nameQueue chan string) {
+	for {
+		nameQueue <- gen.Next()
+	}
+}
+
 // Returns a http.Handler function capable of serving the content of a
 // Generator.
 func NewWebHandler(gen Generator) (func(w http.ResponseWriter, r *http.Request)) {
-
+	
+	nameQueue := make(chan string, 5)
+	go pumpGeneratorIntoChannel(gen, nameQueue)
+	
 	return func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, gen.Next())
+		fmt.Fprintf(w, <- nameQueue )
 	}
 
 }
